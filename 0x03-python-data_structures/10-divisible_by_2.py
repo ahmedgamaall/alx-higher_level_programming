111880be23287b25bd55e0e3a1faa5663632085d
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-12-04 03:26:15 -0800

    create 10-divisible_by_2.py file

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for count, i in enumerate(my_list):
-        if i % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return(boolist)
+    is_even = [num % 2 == 0 for num in my_list]
+    return is_even
