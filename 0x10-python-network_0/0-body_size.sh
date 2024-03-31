203141216fb96791a920a7fbfc296f6c5f1e405a
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-03-31 16:29:19 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # a Bash script that takes in a URL,
 # sends a request to that URL,
 # and displays the size of the body of the response
-echo "curl -s -I "$1"" | grep -i "Content-Length:"
+curl -s -I "$1" | grep -i "Content-Length:"
